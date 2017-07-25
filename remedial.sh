30db729b12a1c99df6eabd9a125a40f2d9257e26
Fhadila <1506164A@student.tp.edu.sg>
2017-07-26 03:37:58 +0800

    Script 6.2.1.15 - 6.2.1.17

--- remedial.sh
+++ remedial.sh
@@ -1,6 +1,61 @@
 # ! /bin/bash
 
-#6.2.1.15
+#6.2.1.15 Collect Successful File System Mounts
+bit64mountb64=`grep "\-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+bit64mountb32=`grep "\-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+bit32mountb32=`grep "\-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+
+if [ -z "$bit64mountb64" ]
+then
+	echo "-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> /etc/audit/audit.rules
+fi
+
+if [ -z "$bit64mountb32" ]
+then
+	echo "-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> /etc/audit/audit.rules
+fi
+
+pkill -HUP -P 1 auditd
+
+if [ -z "$bit32mountb32" ]
+then
+	echo "-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> /etc/audit/audit.rules
+fi
+
+pkill -HUP -P 1 auditd
+
+#2.6.1.16 Collect File Delection Events by User
+bit64delb64=`grep "\-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+bit64delb32=`grep "\-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+bit32delb32=`grep "\-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+
+if [ -z "$bit64delb64" ]
+then
+	echo "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> /etc/audit/audit.rules
+fi
+
+if [ -z "$bit64delb32" ]
+then
+	echo "-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> /etc/audit/audit.rules
+fi
+
+pkill -HUP -P 1 auditd
+
+if [ -z "$bit32delb32" ]
+then
+	echo "-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> /etc/audit/audit.rules
+fi
+
+pkill -P 1 -HUP auditd
+
+#6.2.1.17 Collect Changes to System Administrator Scope
+sudoers=`grep "\-w /etc/sudoers -p wa -k scope" /etc/audit/audit.rules`
+
+if [ -z "$sudoers" ]
+then
+	echo "-w /etc/sudoers -p wa -k scope" >> /etc/audit/audit.rules
+fi
+pkill -HUP -P 1 auditd
 
 #6.2.1.18
 remauditrules=`grep actions /etc/audit/audit.rules`
@@ -12,4 +67,3 @@ then
 fi
 
 pkill -HUP -P 1 auditd
-
--- verify.sh
+++ verify.sh
@@ -1,7 +1,39 @@
 #! /bin/bash
-#6.2.1.14
 
-#6.2.1.15
+#6.2.1.15 Collect Successful File System Mounts
+bit64mountb64=`grep "\-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+bit64mountb32=`grep "\-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+bit32mountb32=`grep "\-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+
+if [ -z "$bit64mountb64" -o -z "$bit64mountb32" -o -z "$bit32mountb32" ]
+then
+	echo "FAIL - To determine filesystem mounts" 
+else
+	echo "PASS - To determine filesystem mounts"
+fi
+
+#6.2.1.16 Collect File Delection Events by User
+bit64delb64=`grep "\-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+bit64delb32=`grep "\-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+bit32delb32=`grep "\-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+
+if [ -z "$bit64delb64" -o -z "$bit64delb32" -o -z "$bit32delb32" ]
+then
+	echo "FAIL - To determine the file delection event by user"
+else
+	echo "PASS - To determine the file delection event by user"
+fi
+
+#6.2.1.17 Collect Changes to System Administration Scope
+chkscope=`grep scope /etc/audit/audit.rules`
+sudoers='-w /etc/sudoers -p wa -k scope'
+
+if [ -z "$chkscope" -o "$chkscope" != "$sudoers" ]
+then
+	echo "FAIL - To unauthorize change to scope of system administrator activity"
+else
+	echo "PASS - To unauthorize change to scope of system administrator activity"
+fi
 
 #6.2.1.18 
 chkauditrules=`grep actions /etc/audit/audit.rules`
@@ -20,5 +52,6 @@ chkmodules=`grep modules /etc/audit/audit.rules`
 
 
 #6.2.1.20
+
 
 
